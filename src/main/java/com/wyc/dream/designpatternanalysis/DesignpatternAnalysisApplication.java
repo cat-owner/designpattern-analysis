361870e6c30b86c1ad09c5dd913bc430d22b7d31
wangyuancun <wangyc5@yonghui.cn>
2022-02-21 20:21:26 +0800

    初始化

---
+++ src/main/java/com/wyc/dream/designpatternanalysis/DesignpatternAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.wyc.dream.designpatternanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignpatternAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignpatternAnalysisApplication.class, args);
+	}
+
+}
